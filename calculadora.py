78a5d4833527c9c6538879469b68e6edc327df75
ludmillaifrn <144167564+ludmillaifrn@users.noreply.github.com>
2025-05-21 08:26:15 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+#calculadora no python com uso de excessões 
+
+n1 = (input("digite um numero "))
+n2 = (input("digite outro numero : "))
+
+ #operações 
+def adição ( n1, n2): 
+    soma = n1 + n2 
+    return soma 
+
+def subtração (n1 , n2): 
+    sub = n1 - n2 
+    return sub 
+
+def multiplicação (n1, n2 ): 
+    multi = n1 * n2 
+    return multi
+
+def divisão (n1 , n2 ):
+    div = n1 / n2 
+    return div 
+
